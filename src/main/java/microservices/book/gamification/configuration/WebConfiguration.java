2d4512e0fed6cd97cfe95cd917374db2e2474aa4
arios <arios@researchinnovations.com>
2022-09-17 10:15:57 -0400

    Chapter 08b - everything worked

--- src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
+++ src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
@@ -1,13 +1,14 @@
-package microservices.book.gamification.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-    }
-}
+//Commented out at chapter 8a (when gateway was addded)
+//package microservices.book.gamification.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//    }
+//}
